dd7ab9a646a41d3b26ac63b2349c949ceae211d4
nguyenminhedutalk <minh@edutalkapp.com>
2020-05-25 08:56:39 +0700

    Init

---
+++ FoodMad.API/Controllers/FoodMadController.cs
@@ -0,0 +1,22 @@
+﻿using FoodMap_Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FoodMad.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodMadController : ControllerBase
+    {
+        [HttpGet("orders/{page}")]
+        public Task<List<Order>> GetListOrder(int page)
+        {
+            var datas = new List<Order>()
+            {
+                new Order(){ }
+            };
+            return Task.FromResult(datas);
+        }
+    }
+}
---
+++ FoodMap_Model/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodMap_Model
+{
+   public class Order
+    {
+        public string IconOrder { set; get; }
+        public string Id { set; get; }
+        int stausAction;
+        public int StatusAction { set; get; }
+        public string OrderTitle { set; get; }
+        public string Price { set; get; }
+        public string Date_Created { set; get; }
+    }
+}
